77f9f6fd3b9a3f03a1b9e7333464a1ee4b9b43f3
jmnf9 <jmnf9@DESKTOP-5NF9P48>
2019-05-12 22:20:27 +0200

    Ejercicio 2 y 3 terminados

--- PCD/PracticasPCD/src/ejercicio2/Ejercicio2.java
+++ PCD/PracticasPCD/src/ejercicio2/Ejercicio2.java
@@ -1,10 +1,131 @@
 package ejercicio2;
 
-public class Ejercicio2 {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.concurrent.Semaphore;
 
+public class Ejercicio2 {
+	
+	public static ArrayList<String> palabras = new ArrayList<String>();		//Lista de todas las palabras de los hilos
+	public static int contador = 0;											//Contador de cuantas palabras llevamos generadas
+	public static Semaphore mutexPantalla = new Semaphore(1);				//Semaforo binario para la exclusion mutua de la pantalla
+	public static Semaphore mutexArray = new Semaphore(1);					//Semaforo binario para la exclusion mutua del array de todas las palabras
+	private static LinkedList<Thread> hilos = new LinkedList<Thread>();		//Lista de hilos para agilizar su ejecución
+
+	public static void main(String[] args) {
+		Thread a = new Hilo("A");
+		Thread b = new Hilo("B");
+		Thread c = new Hilo("C");
+		Thread d = new Hilo("D");
+		Thread e = new Hilo("E");
+		Thread f = new Hilo("F");
+		Thread g = new Hilo("G");
+		Thread h = new Hilo("H");
+		Thread i = new Hilo("I");
+		Thread j = new Hilo("J");
+		Thread k = new Hilo("K");
+		Thread l = new Hilo("L");
+		Thread m = new Hilo("M");
+		Thread n = new Hilo("N");
+		Thread o = new Hilo("O");
+		Thread p = new Hilo("P");
+		Thread q = new Hilo("Q");
+		Thread r = new Hilo("R");
+		Thread s = new Hilo("S");
+		Thread t = new Hilo("T");
+		Thread u = new Hilo("U");
+		Thread v = new Hilo("V");
+		Thread w = new Hilo("W");
+		Thread x = new Hilo("X");
+		Thread y = new Hilo("Y");
+		Thread z = new Hilo("Z");
+		Thread aa = new Hilo("AA");
+		Thread bb = new Hilo("BB");
+		Thread cc = new Hilo("CC");
+		Thread dd = new Hilo("DD");
+		
+		Collections.addAll(hilos, a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,aa,bb,cc,dd);
+		
+		for (Thread hilo : hilos) {
+			hilo.start();
+		}
+		
+		for (Thread hilo : hilos) {
+			try {
+				hilo.join();
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
 	
 }
 
 class Hilo extends Thread {
+	
+	private String id;						//ID del hilo
+	private String palabra;					//Palabra generada por el hilo
+	private ArrayList<String> igualLetra;	//Lista con todas las palabras que empiecen por la misma letra que la nuestra
+	
+	public Hilo(String _id) {
+		id = _id;
+		igualLetra = new ArrayList<String>();
+	}
+	
+	private String generarPalabra() {		//Generador de palabras aleatorias entre 1 y 10 caracteres
+		String palabra = "";
+		int n = (int) ((Math.random()*10)+1);
+		for(int i = 0; i<n; i++) {
+			char c = (char) ((Math.random()*26)+97);
+			palabra += c;
+		}
+		return palabra;
+	}
+	
+	public void run() {
+		
+		palabra = generarPalabra();			//Generamos la palabra
+		
+		try {
+			Thread.sleep((long) (Math.random()*1000));	//Ralentizamos la generacion de la palabra
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
+		
+		try {
+			Ejercicio2.mutexArray.acquire();	//Adquirimos el semaforo del array para añadir la palabra
+		} catch (InterruptedException e) {}
+		Ejercicio2.palabras.add(palabra);		//Añadimos la palabra al array de palabras
+		Ejercicio2.contador++;					//Aumentamos el contador
+		Ejercicio2.mutexArray.release();		//Devolvemos el semaforo
+		
+		while(Ejercicio2.contador!=30) {		//Esperamos a que se generen las 30 palabras
+			try {
+				Thread.sleep((long) (Math.random()*1000));
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			}
+		}
+		
+		for (String pal : Ejercicio2.palabras) { 	//Buscamos las palabras que empiecen por la misma letra que la nuestra
+			if(pal.charAt(0)==palabra.charAt(0)) {
+				igualLetra.add(pal);
+			}
+		}
+		
+		try {
+			Ejercicio2.mutexPantalla.acquire();		//Adquirimos el semaforo de la pantalla para imprimir
+		} catch (InterruptedException e) {}
+		
+		//Imprimimos todo
+		System.out.println("Hilo " + id);
+		System.out.println("Todas las palabras = "+Ejercicio2.palabras.toString());
+		System.out.println("Palabra hilo "+ id + " " + palabra);
+		System.out.println("Palabras que empiezan con mi letra = " + igualLetra.toString());
+		System.out.println("Terminando hilo " + id + "\n");
+		
+		Ejercicio2.mutexPantalla.release();			//Devolvemos el semaforo de pantalla
+	}
 	
 }
--- PCD/PracticasPCD/src/ejercicio3/Ejercicio3.java
+++ PCD/PracticasPCD/src/ejercicio3/Ejercicio3.java
@@ -1,5 +1,237 @@
 package ejercicio3;
 
-public class Ejercicio3 {
+import java.util.concurrent.locks.*;
 
+public class Ejercicio3 {
+	public static void main(String[] args) {
+		Buffer buffer = new Buffer(10);				//Monitor con el buffer
+		
+		Productor1 p11 = new Productor1(buffer);
+		Productor1 p12 = new Productor1(buffer);
+		Productor1 p13 = new Productor1(buffer);
+		Productor2 p21 = new Productor2(buffer);
+		Productor2 p22 = new Productor2(buffer);
+		Productor2 p23 = new Productor2(buffer);
+		Consumidor1 c11 = new Consumidor1(buffer);
+		Consumidor1 c12 = new Consumidor1(buffer);
+		Consumidor1 c13 = new Consumidor1(buffer);
+		Consumidor2 c21 = new Consumidor2(buffer);
+		Consumidor2 c22 = new Consumidor2(buffer);
+		Consumidor2 c23 = new Consumidor2(buffer);
+		
+		p11.start();
+		p12.start();
+		p13.start();
+		p21.start();
+		p22.start();
+		p23.start();
+		c11.start();
+		c12.start();
+		c13.start();
+		c21.start();
+		c22.start();
+		c23.start();
+		
+	}
+}
+
+class Buffer{
+	
+	private ReentrantLock l = new ReentrantLock();		//Cerrojo para la exclusion mutua
+	private Condition productor = l.newCondition();		//Condition para el control de los productores
+	private Condition consumidor = l.newCondition();	//Condition para el control de los consumidores
+	private ElementoB buffer[];							//Buffer
+	private int in,out,tam;								//Variables para controlar por donde hay que devolver objetos y por donde hay que meterlos en el buffer
+	private int tipo1,tipo2;							//Variables que controlan la cantidad de objetos de cada tipo
+	
+	public Buffer(int ele) {					//Inicializacion del monitor
+		buffer = new ElementoB[ele];
+		for(int i=0;i<buffer.length;i++) buffer[i]=null;
+		tipo1=tipo2=in=out=tam=0;
+	}
+	
+	//Funcion que devuelve el tipo del objeto en la cabeza del buffer para saber si un consumidor puede extraer
+	synchronized public int getTipoCabeza() {	
+		if(buffer[out]!=null) return buffer[out].tipo;
+		else return 0;
+	}
+	
+	public void depositar(ElementoB ele) throws InterruptedException {
+		l.lock();			//Cogemos el cerrojo para cumplir la exclusion mutua es igual en todas las funciones
+		try {
+			while(tam>=buffer.length)	//Si el array esta lleno no podemos insertar
+				productor.await();
+			buffer[in]=ele;				//insertamos
+			in=(in+1)%buffer.length;	//Calculamos la nueva posicion donde insertar
+			tam++;						//Aumentamos la variable que controla cuantos elementos llevamos
+			if(ele.tipo==1) tipo1++;	//Si el elemento insertado es de tipo 1 se aumenta su variable
+			else tipo2++;				//si no se aumenta la otra
+			System.out.println("Insertamos Elemento de tipo " + ele.tipo + " Quedan " + getElementos());
+			consumidor.signal();		//Hacemos un signal a un consumidor 
+		}finally {
+			l.unlock();		//Devolvemos el cerrojo, igual en todas las funciones
+		}
+	}
+	
+	public ElementoB extraer1() throws InterruptedException{
+		l.lock();
+		try{
+			while(tipo1==0 || getTipoCabeza()!=1) {		//Si no hay ningun elemento de mi tipo o no esta en la cabeza esperamos
+				consumidor.await();
+			}
+			ElementoB ele=buffer[out];			//Extraemos el elemento
+			buffer[out]=null;					
+			out=(out+1)%buffer.length;			//Calculamos la nueva posicion para extraer
+			tam--;								//Reducimos la cantidad de elementos en el buffer
+			tipo1--;							//Reducimos la cantidad de elementos de este tipo
+			System.out.println("Extraemos Elemento de tipo 1" + " Quedan " + getElementos());
+			productor.signal();					//Llamamos a un productor
+			return ele;							//Devolvemos el elemento
+		}finally{
+			l.unlock();
+		}
+	}
+	
+	public ElementoB extraer2() throws InterruptedException{
+		l.lock();
+		try{
+			while(tipo2==0 || getTipoCabeza()!=2)		//Si no hay ningun elemento de mi tipo o no esta en la cabeza esperamos
+				consumidor.await();
+			ElementoB ele=buffer[out];					//Extraemos el elemento
+			buffer[out]=null;
+			out=(out+1)%buffer.length;					//Calculamos la nueva posicion para extraer
+			tam--;										//Calculamos la nueva posicion para extraer
+			tipo2--;									//Reducimos la cantidad de elementos de este tipo
+			System.out.println("Extraemos Elemento de tipo 2" + " Quedan " + getElementos());
+			productor.signal();							//Llamamos a un productor
+			return ele;									//Devolvemos el elemento
+		}finally{
+			l.unlock();
+		}
+	}
+	
+	//Metodo para obtener cuantos elementos quedan en el buffer
+	public int getElementos() {
+		return tam;
+	}
+}
+
+class Productor1 extends Thread{
+	private Buffer buffer;
+	private static final int TIPO = 1;		//Tipo de productor
+	
+	public Productor1(Buffer b){
+		buffer=b;
+	}
+	
+	public void run(){
+		for(int i=1;i<20;i++){				//Repetimos 20 veces
+			// producir elemento			
+			int item = (int)(Math.random()*10);
+			try{
+				Thread.sleep((int)Math.random()*10);
+			}catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			// depositar elemento
+			try{
+				//Insertamos el elemento en el buffer indicando el tipo
+				buffer.depositar(new ElementoB(item, TIPO));
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+class Productor2 extends Thread{
+	private Buffer buffer;
+	private static final int TIPO = 2;
+	
+	public Productor2(Buffer b){
+		buffer=b;
+	}
+	
+	public void run(){
+		//Repetimos 20 veces
+		for(int i=1;i<20;i++){
+			// producir elemento
+			int item = (int)(Math.random()*10);
+			try{
+				Thread.sleep((int)Math.random()*10);
+			}catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			// depositar elemento
+			try{
+				//Insertamos el elemento en el buffer indicando el tipo
+				buffer.depositar(new ElementoB(item, TIPO));
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+class Consumidor1 extends Thread{
+	private Buffer buffer;
+	
+	public Consumidor1(Buffer b){
+		buffer=b;
+	}
+	
+	public void run(){
+		for(int i=1;i<20;i++){
+			// extraer elemento 
+			try{
+				//Extraemos elemento de nuestro tipo
+				ElementoB item = buffer.extraer1();
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+			// consumir elemento
+			try{
+				Thread.sleep((int)Math.random()*10);
+			}catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+class Consumidor2 extends Thread{
+	private Buffer buffer;
+	
+	public Consumidor2(Buffer b){
+		buffer=b;
+	}
+	
+	public void run(){
+		for(int i=1;i<20;i++){
+			// extraer elemento 
+			try{
+				//Extraemos elemento de nuestro tipo
+				ElementoB item = buffer.extraer2();
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+			// consumir elemento
+			try{
+				Thread.sleep((int)Math.random()*10);
+			}catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+//Definicion de un elemento de buffer
+class ElementoB{
+	int item;
+	int tipo;
+	
+	public ElementoB(int _item, int _tipo) {
+		item=_item;
+		tipo=_tipo;
+	}
 }
