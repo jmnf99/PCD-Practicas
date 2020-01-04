c81e9a67371e9c266c9dfd860d7834b4a94b8adf
jmnf9 <jmnf9@DESKTOP-QGC18K5>
2020-01-04 03:54:04 +0100

    Corregido Ejercicio 4

--- PCD/PracticasPCD/src/ejercicio4/Ejercicio4.java
+++ PCD/PracticasPCD/src/ejercicio4/Ejercicio4.java
@@ -1,6 +1,8 @@
 package ejercicio4;
 
 import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import messagepassing.*;
 
@@ -8,19 +10,33 @@ class Msj implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private int tiempo;
+	private int id;
 	private String impresora;
 	
-	public Msj(int _tiempo, String _impresora) {
+	public Msj(int _tiempo, int _id, String _impresora) {
 		this.tiempo = _tiempo;
+		this.id = _id;
 		this.impresora = _impresora;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public int getTiempo() {
+		return tiempo;
 	}
 	
 	public String getImpresora() {
 		return impresora;
 	}
 	
-	public int getTiempo() {
-		return tiempo;
+	public void setTiempo(int tiempo) {
+		this.tiempo = tiempo;
+	}
+	
+	public void setImpresora(String impresora) {
+		this.impresora = impresora;
 	}
 	
 }
@@ -29,15 +45,15 @@ class Hilo extends Thread {
 	
 	private int id;
 	private MailBox buzonServidor;
-	private MailBox buzon;
+	private MailBox buzonMio;
 	private MailBox buzonFinal;
 	private MailBox buzonMutex;
 	private int tiempo;
 	
-	public Hilo(int _id, MailBox _buzonServidor, MailBox _buzon, MailBox _buzonFinal, MailBox _buzonMutex) {
+	public Hilo(int _id, MailBox _buzonServidor, MailBox _buzonMio, MailBox _buzonFinal, MailBox _buzonMutex) {
 		this.id = _id;
 		this.buzonServidor = _buzonServidor;
-		this.buzon = _buzon;
+		this.buzonMio = _buzonMio;
 		this.buzonFinal = _buzonFinal;
 		this.buzonMutex = _buzonMutex;
 		tiempo = 0;
@@ -48,12 +64,14 @@ class Hilo extends Thread {
 		for(int i=0; i<5; i++) {
 			
 			try {
-				Thread.sleep((int)(Math.random()*1000));			//Generamos un trabajo
+				Thread.sleep((int)(Math.random()*1000));				//Generamos un trabajo
 			} catch (InterruptedException e) {}
+
+			Msj mensaje = new Msj(tiempo, id, null);
 			
-			buzonServidor.send(id);						// Enviamos peticion al servidor
+			buzonServidor.send(mensaje);								// Enviamos peticion al servidor
 			
-			Msj imp = (Msj) buzon.receive();						// Recibimos cual se nos ha otorgado
+			Msj imp = (Msj) buzonMio.receive();							// Recibimos cual se nos ha otorgado
 			
 			tiempo = imp.getTiempo();
 			
@@ -89,6 +107,8 @@ class Servidor extends Thread {
 	private MailBox buzonFinal;
 	private MailBox[] buzonesHilos;
 	private Selector s;
+	private Queue<Msj> colaImpresoraA;
+	private Queue<Msj> colaImpresoraB;
 	
 	
 	public Servidor(MailBox[] _buzonesHilos, MailBox _buzonServidor, MailBox _buzonFinal) {
@@ -100,6 +120,8 @@ class Servidor extends Thread {
 		s = new Selector();
 		s.addSelectable(buzonServidor, false);
 		s.addSelectable(buzonFinal, false);
+		colaImpresoraA = new LinkedList<>();
+		colaImpresoraB = new LinkedList<>();
 	}
 	
 	public void run() {
@@ -107,18 +129,17 @@ class Servidor extends Thread {
 		while(true) {
 			buzonServidor.setGuardValue(true);
 			buzonFinal.setGuardValue(true);
-			System.out.println("Aqui esperando");
-			switch(s.selectOrBlock()) {
-				case 1: int id = (int) buzonServidor.receive();
-					int tiempo = (int)(Math.random()*9+1);
+			
+			//System.out.println("Aqui esperando");
+			switch(s.selectNonBlocking()) {
+				case 1: Msj mensaje = (Msj) buzonServidor.receive();
+					int tiempo = (int)(Math.random()*9+1);					//Calculamos el tiempo que tardará
 					if(tiempo >= 5 ) {
-						Msj mens = new Msj(tiempo, "A");
-						buzonesHilos[id].send(mens);
-						impresoraA = true;
+						mensaje.setTiempo(tiempo);
+						colaImpresoraA.add(mensaje);
 					} else {
-						Msj mens = new Msj(tiempo, "B");
-						buzonesHilos[id].send(mens);
-						impresoraB = true;
+						mensaje.setTiempo(tiempo);						
+						colaImpresoraB.add(mensaje);
 					}
 					break;
 			
@@ -130,24 +151,40 @@ class Servidor extends Thread {
 					}
 					break;
 			}
+			
+			if(!impresoraA && !colaImpresoraA.isEmpty()) {
+				Msj msj = colaImpresoraA.remove();
+				msj.setImpresora("A");
+				impresoraA = true;
+				buzonesHilos[msj.getId()].send(msj);
+			}
+			
+			if(!impresoraB && !colaImpresoraB.isEmpty()) {
+				Msj msj = colaImpresoraB.remove();
+				msj.setImpresora("B");
+				impresoraB = true;
+				buzonesHilos[msj.getId()].send(msj);
+			}
 		}
 	}
 }
 
 public class Ejercicio4 {
+	
+	public static final int NHILOS = 30;
 
 	public static void main(String[] args) {
 		
-		MailBox buzonesHilos[] = new MailBox[30];
+		MailBox buzonesHilos[] = new MailBox[NHILOS];
 		MailBox buzonServidor = new MailBox();
 		MailBox buzonFinal = new MailBox();
 		MailBox buzonMutex = new MailBox();
 		
-		Hilo hilos[] = new Hilo[30];
+		Hilo hilos[] = new Hilo[NHILOS];
 		
-		for(int i=0; i<30; i++) {
+		for(int i=0; i<NHILOS; i++) {
 			buzonesHilos[i] = new MailBox();
-			hilos[i] = new Hilo(i, buzonesHilos[i], buzonServidor, buzonFinal, buzonMutex);
+			hilos[i] = new Hilo(i, buzonServidor, buzonesHilos[i], buzonFinal, buzonMutex);
 		}
 		
 		Servidor serv = new Servidor(buzonesHilos, buzonServidor, buzonFinal);
